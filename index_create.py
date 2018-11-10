df60bf5184a60444d5bcb41faee9cd328fa69aea
Shuai-Hsun <hellovictorlee@gmail.com>
2018-11-09 18:30:18 -0800

    initializing

---
+++ index_create.py
@@ -0,0 +1,10 @@
+"""indexing multiple pdf file
+usage: python3 index_creator.py
+default setting: index all files under the current directory, and create file\
+        called index.txt
+
+argstype: None
+retype: text file
+"""
+import PyPDF2
+import os
