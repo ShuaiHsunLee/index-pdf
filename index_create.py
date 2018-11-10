c9dbbb0cd1bce765925308e099b15f10ebd6552f
Shuai-Hsun <hellovictorlee@gmail.com>
2018-11-09 23:05:04 -0800

    initializing

--- index_create.py
+++ index_create.py
@@ -6,5 +6,18 @@ default setting: index all files under the current directory, and create file\
 argstype: None
 retype: text file
 """
-import PyPDF2
-import os
+from PyPDF2 import PdfFileReader
+from pdb import set_trace
+
+
+def get_content(path):
+    with open(path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        page = pdf.getPage(0)
+        # set_trace()
+        text = page.extractText()
+        print(text)
+
+
+if __name__ == '__main__':
+    get_content('./pdf/Resume_Victor.pdf')
